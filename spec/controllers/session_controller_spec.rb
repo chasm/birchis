9ff55178b7f07440af00df3fa82a32a9797711f5
chasm <charles.munat@gmail.com>
2013-07-16 16:21:52 -0700

    Added examples for session_controller_spec.

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,13 +1,34 @@
 require 'spec_helper'
 
 describe SessionController do
-  subject(:user) { Fabricate(:user) }
+  let!(:email) { "sam@stone.com" }
+  let!(:pw) { "123xyz" }
+  
+  subject!(:user) { Fabricate(
+    :user,
+    email: email,
+    password: pw,
+    password_confirmation: pw
+  )}
   
   describe "POST #create" do
-    it "logs an authenticated user in and redirects to site#index"
+    it "logs an authenticated user in and redirects to site#index" do
+      post("create", email: email, password: pw)
+
+      expect(response).to redirect_to(root_url)
+    end
     
-    it "redisplays the login form for authentication failures"
+    it "redisplays the login form for authentication failures" do
+      post("create", email: email, password: "...")
+
+      expect(response).to render_template("new")
+    end
     
-    it "sets code and expires_at for a password reset request"
+    it "sets code and expires_at for a password reset request" do
+      post("create", email: email, password: "")
+
+      expect(user.code).not_to be_nil
+      expect(user.expires_at).not_to be_nil
+    end
   end
 end
