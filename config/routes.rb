85ed5690b751fe9ac5747a2ee8eea69612eae37f
chasm <charles.munat@gmail.com>
2013-07-15 17:42:47 -0700

    Finished basic authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Birchis::Application.routes.draw do
   get "login" => "session#new"
   post "login" => "session#create"
   delete "logout" => "session#destroy"
+  get "logout" => "session#destroy"
   
   root 'site#index'
 end
