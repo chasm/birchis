45fc1c9dca811c15cd3c926cf88146b9d2c141fe
chasm <charles.munat@gmail.com>
2013-07-17 15:44:34 -0700

    Added Users controller and listed users.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    redirect_to users_path, status: 303
+  end
+
+  def update
+    redirect_to users_path, status: 303
+  end
+
+  def destroy
+    redirect_to users_path, status: 303
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Birchis::Application.routes.draw do
   get "privacy" => "site#privacy"
   get "terms" => "site#terms"
   
-  resource :session, only: [ :new, :create, :destroy ]
+  resource :session , only: [ :new, :create, :destroy ]
   
   get "login" => "session#new"
   post "login" => "session#create"
   delete "logout" => "session#destroy"
   get "logout" => "session#destroy"
+  
+  resources :users
   
   root 'site#index'
 end
